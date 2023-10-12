631d509ef281f2f13402c5d58ce7a0bddce7286b
pascal Zhang <pascalzhang@MacBook-Pro-de-pascal.local>
2023-10-12 08:05:34 +0200

    44-r3f-drei

---
+++ 44-r3f-drei/src/Experience.jsx
@@ -0,0 +1,78 @@
+import { useRef } from "react";
+import {
+  Html,
+  MeshReflectorMaterial,
+  Float,
+  Text,
+  TransformControls,
+  OrbitControls,
+  PivotControls,
+} from "@react-three/drei";
+
+export default function Experience() {
+  const cubeRef = useRef();
+  const sphereRef = useRef();
+
+  return (
+    <>
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        axisColors={["#9381ff", "#fff4d6d", "#7ae582"]}
+        scale={100}
+        fixed={true}
+      >
+        <mesh position-x={-2} ref={sphereRef}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+          <Html
+            position={[1, 1, 0]}
+            wrapperClass="label"
+            center
+            distanceFactor={6}
+            occlude={[sphereRef, cubeRef]}
+          >
+            That a sphere 🎉
+          </Html>
+        </mesh>
+      </PivotControls>
+
+      <mesh position-x={2} scale={1.5} ref={cubeRef}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <TransformControls object={cubeRef} mode="translate" />
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        {/* <meshStandardMaterial color="greenyellow" /> */}
+        <MeshReflectorMaterial
+          color="greenyellow"
+          resolution={512}
+          blur={[1000, 1000]}
+          mixBlur={1}
+          mirror={0.75}
+        />
+      </mesh>
+      <Float speed={5} floatIntensity={2}>
+        <Text
+          font="./bangers-v20-latin-regular.woff"
+          fontSize={1}
+          color="salmon"
+          position-y={2}
+          maxWidth={2}
+          textAlign="center"
+        >
+          I LOVE R3F
+        </Text>
+      </Float>
+    </>
+  );
+}
