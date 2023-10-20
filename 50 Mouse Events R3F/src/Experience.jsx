40b2b865298ffa6feca66e67a3e4e0226d1410b1
pascal Zhang <pascalzhang@MacBook-Pro-de-pascal.local>
2023-10-20 08:58:59 +0200

    51-Post-processing R3F

--- 50 Mouse Events R3F/src/Experience.jsx
+++ 50 Mouse Events R3F/src/Experience.jsx
@@ -1,37 +1,111 @@
-import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
+import { useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  useCursor,
+  useGLTF,
+  meshBounds,
+} from "@react-three/drei";
+import { useRef, useState } from "react";
+import * as THREE from "three";
 
-export default function Experience()
-{
-    const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
-        cube.current.rotation.y += delta * 0.2
-    })
+export default function Experience() {
+  const [pointer, setPointer] = useState(false);
+  const cube = useRef();
+  useCursor(pointer);
 
-    return <>
+  const hamburger = useGLTF("./hamburger.glb");
 
-        <OrbitControls makeDefault />
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta * 0.2;
+  });
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+  const eventHandler = (event) => {
+    event.stopPropagation();
+    cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`);
+    console.log("event occured");
+  };
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+  const shpereEventHandler = (event) => {
+    event.stopPropagation();
+  };
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+  return (
+    <>
+      <OrbitControls makeDefault />
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
+      <mesh
+        position-x={-2}
+        // to create a sphere that react to mouse interaction, it give better performance.
+        raycast={meshBounds}
+        onClick={shpereEventHandler}
+        // onPointerEnter={() => {
+        //   document.body.style.cursor = "pointer";
+        // }}
+        // onPointerLeave={() => {
+        //   document.body.style.cursor = "default";
+        // }}
+        onPointerEnter={() => {
+          setPointer(true);
+        }}
+        onPointerLeave={() => {
+          setPointer(false);
+        }}
+      >
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh
+        ref={cube}
+        position-x={2}
+        scale={1.5}
+        onClick={eventHandler}
+        // onContextMenu is the right click
+        // onContextMenu={eventHandler}
+        // onDoubleClick={eventHandler}
+        // onPointerUp={eventHandler}
+        // onPointerDown={eventHandler}
+        // onPointerOver={eventHandler}
+        // onPointerEnter={eventHandler}
+        // onPointerOut={eventHandler}
+        // onPointerLeave={eventHandler}
+        // onPointerMove={eventHandler}
+        // onPointerMissed={eventHandler}
+        // onPointerEnter={() => {
+        //   document.body.style.cursor = "pointer";
+        // }}
+        // onPointerLeave={() => {
+        //   document.body.style.cursor = "default";
+        // }}
+        onPointerEnter={() => {
+          setPointer(true);
+        }}
+        onPointerLeave={() => {
+          setPointer(false);
+        }}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" side={THREE.DoubleSide} />
+      </mesh>
+
+      <primitive
+        object={hamburger.scene}
+        scale={0.25}
+        position-y={0.5}
+        onClick={(e) => {
+          e.stopPropagation();
+          console.log(e.object.name);
+          console.log("lcikc");
+        }}
+      />
     </>
+  );
 }
